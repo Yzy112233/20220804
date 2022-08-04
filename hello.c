8b0e250034c6c222bd8f025dfd270cd2fe6fa5c9
Yzy112233 <110577775+Yzy112233@users.noreply.github.com>
2022-08-04 13:47:09 +0800

    Delete hello.c

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(int argc, const char *argv[])
-{
-	printf("hello world\n");
-	printf("hello yyz\n");
-	printf("hello develop\n");
-        printf("hello windows\n");
-	return 0;
-}
